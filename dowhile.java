93f373f82b3af42ee52c139046413fd6306cf915
dennisnderitu254 <dknderitu@gmail.com>
2018-10-08 21:48:54 +0300

    control flow in java

---
+++ dowhile.java
@@ -0,0 +1,20 @@
+import java.util.scanner;
+
+class DoWhile{
+
+	/*
+		Do while loop executes statements until certain conditions become false
+	*/
+
+	public static void main(String[] args){
+
+		int i=0;
+
+	do
+	{
+		System.out.println("i is :" + i);
+		i++;
+	}while(i<5);
+
+	}
+}
