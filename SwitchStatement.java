26ac65d217ece005ffef62d86711e822fe341f9e
dennisnderitu254 <dknderitu@gmail.com>
2018-10-08 21:49:59 +0300

    the switch statement in java

---
+++ SwitchStatement.java
@@ -0,0 +1,31 @@
+public class SwitchStatement{
+	
+
+	public static void main(String [] args){
+
+		for(int i=0; i <= 3; i++)
+		{
+			switch (i)
+			{
+				case 0:
+				System.out.println("i is 0");
+				break;
+
+				case 1:
+				System.out.println(" i is 1");
+				break;
+
+				case 2:
+				System.out.println(" i is 2");
+				break;
+
+				default:
+				System.out.println("i is greater than 2");
+
+
+			}
+		}
+	}
+
+
+}
